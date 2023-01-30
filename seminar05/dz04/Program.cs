769a296163f7ceaa57a8317c4451e7d2689e66e2
KonstantinManuylov <konstantinmanuijlov@gmail.com>
2023-01-30 23:31:11 +0400

    Reverse the order of words from string - done

---
+++ seminar05/dz04/Program.cs
@@ -0,0 +1,52 @@
+﻿// перед вами строка, "Например вот такая строка" 
+// надо реверснуть строки на выходе "строка такая вот Например"
+
+string text = "Например вот такая строка";
+string[] textArray = TextToArray(text);
+PrintReversedArray(textArray);
+string[] reversedArray = Reverse(textArray);
+PrintArray(reversedArray);
+
+string[] TextToArray(string text)
+{
+    string[] result = text.Split(" ");
+    return result;
+}
+
+// Вариант просто распечатки строкового массива в обратную сторону.
+
+void PrintReversedArray(string[] array) 
+{
+    Console.WriteLine($"Метод распечатки массива циклом в обратную сторону: ");
+    for (int i = array.Length -1; i >= 0; i--)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"Метод реверса массива кортежами: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+// Вариант реверса массива кортежами, спасибо семинару :)
+
+string[] Reverse(string[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        Swap(array, i, array.Length - 1 - i);
+    }
+    return array;
+}
+
+void Swap(string[] array, int i, int j)
+{
+    (array[i], array[j]) = (array[j], array[i]);
+}
