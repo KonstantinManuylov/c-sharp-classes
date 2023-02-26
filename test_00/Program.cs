b0d5fc4731134347a2a17703d0dd2ea1df6a90fd
KonstantinManuylov <konstantinmanuijlov@gmail.com>
2023-02-26 23:08:29 +0400

    Wrote the funcion TruePrint

---
+++ test_00/Program.cs
@@ -0,0 +1,12 @@
+﻿string TruePrint(string[] array)
+{
+    string res = string.Empty;
+    int size = array.Length;
+    res = "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        res += $"{array[i],3} ";
+    }
+    res += "]";
+    return res;
+}
