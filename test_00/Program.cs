b2ce48cf2fd322f6ecd135abf846b5360e43b7c7
KonstantinManuylov <konstantinmanuijlov@gmail.com>
2023-02-26 23:10:37 +0400

    Made NewWords function

--- test_00/Program.cs
+++ test_00/Program.cs
@@ -21,3 +21,19 @@ int Range(string[] array, int charAmount = 3)
     }
     return count;
 }
+
+string[] NewWords(string[] array, int charAmount = 3)
+{
+    string[] finishArray = new string[Range(array)];
+    int index = 0;
+    // string temp = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= charAmount)
+        {
+            finishArray[index] = array[i];
+            index++;
+        }
+    }
+    return finishArray;
+}
